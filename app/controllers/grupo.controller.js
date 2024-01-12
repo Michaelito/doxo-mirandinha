33a1018dcb833d6607fb148b036feb9c4fbda067
karol.jozef <karoljozef123@hotmail.com>
2024-01-11 22:49:26 -0300

    Finalizado o Crud de Grupos

--- app/controllers/grupo.controller.js
+++ app/controllers/grupo.controller.js
@@ -2,7 +2,33 @@ const db = require("../models");
 const Grupo = db.grupos;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
+// Create and Save a new Group
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.name) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
+
+    // Create a Group
+    const grupo = {
+        name: req.body.name,
+        description: req.body.description        
+    };
+
+    // Save Group in the database
+    Grupo.create(grupo)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Group."
+            });
+        });
+};
 
 // Retrieve all from the database.
 exports.findAll = (req, res) => {
@@ -29,5 +55,86 @@ exports.findAll = (req, res) => {
                 message: "The request has not succeeded",
                 data: null
             }).status(500);
+        });
+};
+
+// Find a single Group with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Grupo.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Group with id=" + id
+            });
+        });
+};
+
+// Update a Group by the id in the request
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Grupo.update(req.body, {
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Group was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Group with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Grupo with id=" + id
+            });
+        });
+};
+
+// Delete a Group with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Grupo.destroy({
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Group was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Group with id=${id}. Maybe Tutorial was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Group with id=" + id
+            });
+        });
+};
+
+// Delete all Groups from the database.
+exports.deleteAll = (req, res) => {
+    Grupo.destroy({
+            where: {},
+            truncate: false
+        })
+        .then(nums => {
+            res.send({ message: `${nums} Groups were deleted successfully!` });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while removing all groups."
+            });
         });
 };
--- app/routes/api/v1/grupo.routes.js
+++ app/routes/api/v1/grupo.routes.js
@@ -5,8 +5,23 @@ module.exports = app => {
 
     var router = require("express").Router();
 
+    // // Create a new Grupos
+    router.post("/", controller.create);
+
     // Retrieve all controller
     router.get("/", controller.findAll);
+
+     // Retrieve a single Grupo with id
+     router.get("/:id", controller.findOne);
+
+     // Update a Grupo with id
+    router.put("/:id", controller.update);
+
+     // Delete a Grupo with id
+     router.delete("/:id", controller.delete);
+
+      // Delete all controller
+    router.delete("/", controller.deleteAll);
 
     app.use('/api/v1/grupo', router);
 
