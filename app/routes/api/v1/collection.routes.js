ecd0967d824f80f86b06cf23572bb68d7b71e29e
karol.jozef <karoljozef123@hotmail.com>
2024-01-15 22:27:36 -0300

    Feito Crud de Coleções

--- app/routes/api/v1/collection.routes.js
+++ app/routes/api/v1/collection.routes.js
@@ -4,11 +4,24 @@ module.exports = app => {
     const controller = require("../../../controllers/collection.controller.js");
 
     var router = require("express").Router();
-    
+
     // Retrieve all controller
     router.get("/", controller.findAll);
-    
+
+    // Retrieve a single Data with id
+    router.get("/:id", controller.findOne);
+
+    //Create
     router.post("/", controller.create);
+
+    // Update a Data with id
+    router.put("/:id", controller.update);
+
+    // Delete a Data with id
+    router.delete("/:id", controller.delete);
+
+    //Delete all 
+    router.delete("/", controller.deleteAll);
 
     app.use('/api/v1/collection', router);
 
